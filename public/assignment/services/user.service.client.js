32b6fc05ae48599d854dc8d64d45162262306c0a
Chris Clark <c.clark1928@gmail.com>
2016-02-26 14:27:52 -0500

    Create UserService and add to index

---
+++ public/assignment/services/user.service.client.js
@@ -0,0 +1,57 @@
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .factory("UserService", UserService);
+
+    function UserService() {
+
+    	var users = [
+		  {	"_id":123, "firstName":"Alice",            "lastName":"Wonderland",
+			"username":"alice",  "password":"alice",   "roles": ["student"]		},
+		  {	"_id":234, "firstName":"Bob",              "lastName":"Hope",
+			"username":"bob",    "password":"bob",     "roles": ["admin"]		},
+		  {	"_id":345, "firstName":"Charlie",          "lastName":"Brown",
+			"username":"charlie","password":"charlie", "roles": ["faculty"]		},
+		  {	"_id":456, "firstName":"Dan",              "lastName":"Craig",
+			"username":"dan",    "password":"dan",     "roles": ["faculty", "admin"]},
+		  {	"_id":567, "firstName":"Edward",           "lastName":"Norton",
+			"username":"ed",     "password":"ed",      "roles": ["student"]		}
+		];
+
+		var api = {
+			findUserByCredentials: findUserByCredentials,
+			findAllUsers: findAllUsers,
+			createUser: createUser,
+			deleteUserById: deleteUserById,
+			updateUser: updateUser
+		};
+
+		return api;
+
+        function findUserByCredentials(username, password, callback) {
+            for (var i = 0; i < users.length; i++) {
+            	if (users[i].username == username && users[i].password == password) {
+            		callback(users[i]);
+            		return;
+            	}
+            };
+            callback(null);
+        }
+
+        function findAllUsers(callback) {
+            return users;
+        }
+
+        function createUser(user, callback) {
+            
+        }
+
+        function deleteUserById(userId, callback) {
+            
+        }
+
+        function updateUser(userId, user, callback) {
+            
+        }
+    }
+})();
