6ef96959fa4aec13e3ded0adf75587dfa20d7845
Chris Clark <c.clark1928@gmail.com>
2016-02-26 20:03:33 -0500

    Implement login controller and view functionality

--- public/assignment/views/users/login.controller.js
+++ public/assignment/views/users/login.controller.js
@@ -3,7 +3,23 @@
         .module("FormBuilderApp")
         .controller("LoginController", LoginController);
 
-    function LoginController() {
-        //TODO
+    function LoginController($rootScope, $scope, $location, UserService) {
+        // Inject login function into scope
+        $scope.login = login;
+
+    	// Controller variables bound to view inputs
+        $scope.username;
+        $scope.password;
+
+  		// Attempt to login with input values
+		// If the user exists, it's stored in the $rootScope and user is re-routed to profile page
+        function login() {
+        	UserService.findUserByCredentials($scope.username, $scope.password, function(response) {
+        		if (response) {
+        			$rootScope.currentUser = response;
+        			$location.path("#/profile");
+        		}
+        	});
+        }
     }
 })();
