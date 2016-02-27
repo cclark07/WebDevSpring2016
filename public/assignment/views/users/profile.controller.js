63b53e250851dc69b1cf8feb0440afb19bea7c2c
Chris Clark <c.clark1928@gmail.com>
2016-02-26 19:48:38 -0500

    Implement profile update form

--- public/assignment/views/users/profile.controller.js
+++ public/assignment/views/users/profile.controller.js
@@ -4,21 +4,29 @@
         .controller("ProfileController", ProfileController);
 
     function ProfileController($rootScope, $scope, UserService) {
+
+        // Inject update function into scope
+        $scope.update = update;
+
     	// Get currentUser from rootScope
     	var user = $rootScope.currentUser;
 
-       // Inject update function into scope
-    	$scope.update = update;
+    	// Update controller variables bound to view inputs with currentUser data
+        $scope.username = user.username;
+        $scope.password = user.password;
+        $scope.firstname = user.firstname;
+        $scope.lastname = user.lastname;
+        $scope.email = user.email;
 
-    	// Controller variables to be bound to view inputs
-        $scope.username;
-        $scope.password;
-        $scope.firstname;
-        $scope.lastname;
-        $scope.email;
-
+        // Updates the current user
         function update() {
-        	
+            user.username = $scope.username;
+            user.password = $scope.password;
+            user.firstname = $scope.firstname;
+            user.lastname = $scope.lastname;
+            user.email = $scope.email;
+
+        	UserService.updateUser(user._id, user, function(x) {console.log(x)});
         }
     }
 })();
