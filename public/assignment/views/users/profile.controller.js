1db538059a3d944703ac7b47d90dc89d48e451be
Chris Clark <c.clark1928@gmail.com>
2016-02-26 19:30:07 -0500

    Set up profile view and controller

--- public/assignment/views/users/profile.controller.js
+++ public/assignment/views/users/profile.controller.js
@@ -3,7 +3,22 @@
         .module("FormBuilderApp")
         .controller("ProfileController", ProfileController);
 
-    function ProfileController() {
-        //TODO
+    function ProfileController($rootScope, $scope, UserService) {
+    	// Get currentUser from rootScope
+    	var user = $rootScope.currentUser;
+
+       // Inject update function into scope
+    	$scope.update = update;
+
+    	// Controller variables to be bound to view inputs
+        $scope.username;
+        $scope.password;
+        $scope.firstname;
+        $scope.lastname;
+        $scope.email;
+
+        function update() {
+        	
+        }
     }
 })();
