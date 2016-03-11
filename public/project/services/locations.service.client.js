5daccdce461080d6a322a0a4fb504695853584e4
Chris Clark <c.clark1928@gmail.com>
2016-03-11 11:31:14 -0500

    Setup location and users services

--- public/project/services/locations.service.client.js
+++ public/project/services/locations.service.client.js
@@ -5,6 +5,96 @@
         .factory("LocationService", LocationService);
 
     function LocationService() {
-    	//TODO
+        var locations;
+
+        locations = [
+            {"_id": "1",
+                "name": "Boston",
+                "userId": 1,
+                "latlon": [42, -71],
+                "webcamURL": "http://test.test.test",
+                "weatherURL": "http://test.test.test"
+            },
+            {"_id": "2",
+                "name": "Weymouth",
+                "userId": 1,
+                "latlon": [40, -72],
+                "webcamURL": "http://test.test.test",
+                "weatherURL": "http://test.test.test"
+            },
+            {"_id": "3",
+                "name": "Long Island",
+                "userId": 2,
+                "latlon": [40, -73],
+                "webcamURL": "http://test.test.test",
+                "weatherURL": "http://test.test.test"
+            },
+            {"_id": "4",
+                "name": "Grand Cayman",
+                "userId": 3,
+                "latlon": [19, -81],
+                "webcamURL": "http://brucesbythesea.dyndns.org:8081/oneshotimage1?1457713199601",
+                "weatherURL": "http://test.test.test"
+            }
+        ];
+
+        var api = {
+            createLocationForUser: createLocationForUser,
+            findAllLocationsForUser: findAllLocationsForUser,
+            deleteLocationById: deleteLocationById,
+            updateLocationById: updateLocationById,
+        };
+
+        return api;
+
+        // Adds property called _id with a timestamp as a unique ID
+        // Adds property called userId equal to user id parameter
+        // Adds new location to local array of locations
+        // Calls back with new location
+        function createLocationForUser(userId, location, callback) {
+            location._id = (new Date).getTime();
+            location.userId = userId;
+            locations.push(location);
+            callback(location);
+        }
+
+        // Iterates over the array of current locations looking for locations whose user id is parameter user id
+        // Calls back with found locations for user id parameter, empty array otherwise
+        function findAllLocationsForUser(userId, callback) {
+            var userLocations = [];
+            for (var i = 0; i < locations.length; i++) {
+                if (locations[i].userId == userId) {
+                    userLocations.push(locations[i]);
+                }
+            };
+            callback(userLocations);
+        }
+
+        // Iterates over array of locations looking for location whose id is location id parameter
+        // If found, removes location from current array of locations
+        // Calls back with remaining array of locations
+        function deleteLocationById(locationId, callback) {
+            for (var i = 0; i < locations.length; i++) {
+                if (locations[i]._id == locationId) {
+                    locations.splice(i, 1);
+                    break;
+                }
+            };
+            callback(locations);
+        }
+
+        // Iterates over array of locations looking for location whose id is location id parameter
+        // If found, updates location object with new location values and calls back with update location
+        // If not found, calls back with null
+        function updateLocationById(locationId, newLocation, callback) {
+            for (var i = 0; i < locations.length; i++) {
+                if (locations[i]._id == locationId) {
+                    locations[i] = newLocation;
+                    callback(locations[i]);
+                    return;
+                }
+            };
+            callback(null);
+        }
     }
 })();
--- public/project/views/users/profile.controller.js
+++ public/project/views/users/profile.controller.js
@@ -4,12 +4,39 @@
         .module("SourceCamApp")
         .controller("ProfileController", ProfileController);
 
-    function ProfileController($rootScope, $scope, $location) {
-        //TODO
+    function ProfileController($rootScope, $scope, $location, UserService) {
         $scope.newLocation = newLocation;
+        $scope.update = update;
+
+        // Get currentUser from rootScope
+        var user = $rootScope.currentUser;
+
+        // Update controller variables bound to view inputs with currentUser data
+        $scope.username = user.username;
+        $scope.password = user.password;
+        $scope.firstname = user.firstName;
+        $scope.lastname = user.lastName;
+        $scope.email = user.email;
+        $scope.locations = user.locations;
+
+        // Updates the current user
+        function update() {
+            user.username = $scope.username;
+            user.password = $scope.password;
+            user.firstName = $scope.firstname;
+            user.lastName = $scope.lastname;
+            user.email = $scope.email;
+
+            UserService.updateUser(user._id, user, function(response) {
+                $rootScope.currentUser = response;
+                user = response;
+            });
+        }
 
         function newLocation() {
             $location.url("/create");
         }
+
+
     }
 })();
