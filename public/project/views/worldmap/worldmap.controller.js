7822f45958d73d875d9d75e7f71b3733757a5bc3
Chris Clark <c.clark1928@gmail.com>
2016-03-11 15:50:12 -0500

    Reload map when on home page, remove inline styles

--- public/project/views/worldmap/worldmap.controller.js
+++ public/project/views/worldmap/worldmap.controller.js
@@ -4,16 +4,20 @@
         .module("SourceCamApp")
         .controller("MapController", MapController);
 
-    function MapController() {
+    function MapController($location) {
         function initMap() {
             var mapProp = {
                 center:new google.maps.LatLng(42.508742,-71.120850),
                 zoom:2,
-                mapTypeId:google.maps.MapTypeId.TERRAIN
+                mapTypeId:google.maps.MapTypeId.HYBRID
             };
             var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
         }
         google.maps.event.addDomListener(window, 'load', initMap);
+
+        if ($location.url() == '/home') {
+            initMap();
+        }
     }
 })();
 
