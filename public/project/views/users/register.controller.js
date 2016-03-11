9ff521738d09026758e2c344cc5544e18148fc89
Chris Clark <c.clark1928@gmail.com>
2016-03-11 11:02:25 -0500

    Implement user registration

--- public/project/views/users/register.controller.js
+++ public/project/views/users/register.controller.js
@@ -4,7 +4,34 @@
         .module("SourceCamApp")
         .controller("RegisterController", RegisterController);
 
-    function RegisterController($rootScope, $scope, $location) {
-        //TODO
+    function RegisterController($rootScope, $scope, $location, UserService) {
+        // Inject register function into scope
+        $scope.register = register;
+
+        // Controller variables to be bound to view inputs
+        $scope.username;
+        $scope.password;
+        $scope.verify;
+        $scope.firstname;
+        $scope.lastname;
+        $scope.email;
+
+        // Uses the UserService to create the new user
+        // Store the new user object in the $rootScope as currentUser
+        // Use the $location service to navigate to the profile view
+        function register() {
+            var newUser = {
+                "username":$scope.username,
+                "password":$scope.password,
+                "firstName":$scope.firstname,
+                "lastName":$scope.lastname,
+                "email":$scope.email,
+                "roles": ["User"]
+            };
+            UserService.createUser(newUser, function(response) {
+                $rootScope.currentUser = response;
+                $location.path("/profile");
+            })
+        }
     }
 })();
