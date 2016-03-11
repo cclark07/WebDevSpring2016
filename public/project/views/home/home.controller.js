de3465265a606ee92fe7a13aaeb31a5257ae85a2
Chris Clark <c.clark1928@gmail.com>
2016-03-11 15:00:38 -0500

    Populate search results with all locations

--- public/project/views/home/home.controller.js
+++ public/project/views/home/home.controller.js
@@ -4,7 +4,23 @@
         .module("SourceCamApp")
         .controller("HomeController", HomeController);
 
-    function HomeController() {
-        //TODO
+    function HomeController($scope, LocationService) {
+        $scope.locations = [];
+
+        // Inject functions into scope
+        $scope.searchLocations = searchLocations;
+
+        $scope.locationName;
+        $scope.userId
+        $scope.latlon;
+        $scope.webcamURL;
+        $scope.weatherURL;
+        $scope.status;
+
+        function searchLocations() {
+            LocationService.getAllLocations(function(response) {
+                $scope.locations = response;
+            })
+        }
     }
 })();
