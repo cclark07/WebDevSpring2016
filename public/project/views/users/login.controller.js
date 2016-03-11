0330dc0972d4eb0795d0af244d9c96713cdb57e5
Chris Clark <c.clark1928@gmail.com>
2016-03-11 10:53:05 -0500

    Implement login

--- public/project/views/users/login.controller.js
+++ public/project/views/users/login.controller.js
@@ -4,7 +4,23 @@
         .module("SourceCamApp")
         .controller("LoginController", LoginController);
 
-    function LoginController($rootScope, $scope, $location) {
-        //TODO
+    function LoginController($rootScope, $scope, $location, UserService) {
+        // Inject login function into scope
+        $scope.login = login;
+
+        // Controller variables bound to view inputs
+        $scope.username;
+        $scope.password;
+
+        // Attempt to login with input values
+        // If the user exists, it's stored in the $rootScope and user is re-routed to profile page
+        function login() {
+            UserService.findUserByCredentials($scope.username, $scope.password, function(response) {
+                if (response) {
+                    $rootScope.currentUser = response;
+                    $location.path("/profile");
+                }
+            });
+        }
     }
 })();
