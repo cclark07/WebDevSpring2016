6efff12799d8030f91abcad96d37e2297e193ee7
Chris Clark <c.clark1928@gmail.com>
2016-03-11 13:32:40 -0500

    Allow admin to create users

--- public/project/views/users/users.controller.js
+++ public/project/views/users/users.controller.js
@@ -7,14 +7,37 @@
     function UsersController($scope, UserService) {
         $scope.users = [];
 
+        init();
+
+        // Inject functions into scope
+        $scope.addUser = addUser;
+
+        $scope.username;
+        $scope.password;
+        $scope.firstName;
+        $scope.lastName;
+        $scope.email;
+        $scope.roles;
+
         function init() {
             UserService.findAllUsers(function(response) {
                 $scope.users = response;
             })
         }
 
-        init();
+        // Uses the UserService to create the new user
+        function addUser() {
+            var newUser = {
+                "username":$scope.username,
+                "password":$scope.password,
+                "firstName":$scope.firstName,
+                "lastName":$scope.lastName,
+                "email":$scope.email,
+                "roles":$scope.roles
+            };
+            UserService.createUser(newUser, function(response) {
 
-
+            })
+        }
     }
 })();
