889c65f72d101e08e19f4077e83cd4343409f33e
Chris Clark <c.clark1928@gmail.com>
2016-03-18 17:08:34 -0400

    Implement adding field to form

--- public/assignment/client/views/forms/fields.controller.js
+++ public/assignment/client/views/forms/fields.controller.js
@@ -4,7 +4,7 @@
         .module("FormBuilderApp")
         .controller("FieldsController", FieldsController);
 
-    function FieldsController(FieldsService, FormService, $routeParams) {
+    function FieldsController(FieldsService, $routeParams) {
         var vm = this;
 
         vm.fieldType = null;
@@ -57,16 +57,77 @@
 
             var field = null;
             if (fieldTypeValue) {
-                field = {
-                    label: "",
-                    type: fieldTypeValue,
-                    placeholder: "",
-                    options: null
-                };
+                switch(fieldTypeValue) {
+                    case "TEXT":
+                        field = {
+                            _id: null,
+                            formId: formId,
+                            label: "New Text Field",
+                            type: "TEXT",
+                            placeholder: "New Field"
+                        };
+                        break;
+                    case "TEXTAREA":
+                        field = {
+                            _id: null,
+                            formId: formId,
+                            label: "New Text Field",
+                            type: "TEXTAREA",
+                            placeholder: "New Field"
+                        };
+                        break;
+                    case "DATE":
+                        field = {
+                            _id: null,
+                            formId: formId,
+                            label: "New Date Field",
+                            type: "DATE",
+                        };
+                        break;
+                    case "OPTIONS":
+                        field = {
+                            _id: null,
+                            formId: formId,
+                            label: "New Dropdown",
+                            type: "OPTIONS",
+                            options: [
+                                {label: "Option 1", value: "OPTION 1"},
+                                {label: "Option 2", value: "OPTION 2"},
+                                {label: "Option 3", value: "OPTION 3"}
+                            ]
+                        };
+                        break;
+                    case "CHECKBOXES":
+                        field = {
+                            _id: null,
+                            formId: formId,
+                            label: "New Checkboxes",
+                            type: "CHECKBOXES",
+                            options: [
+                                {label: "Option A", value: "OPTION A"},
+                                {label: "Option B", value: "OPTION B"},
+                                {label: "Option C", value: "OPTION C"}
+                            ]
+                        };
+                        break;
+                    case "RADIOS":
+                        field = {
+                            _id: null,
+                            formId: formId,
+                            label: "New Radio Buttons",
+                            type: "RADIOS",
+                            options: [
+                                {label: "Option X", value: "OPTION X"},
+                                {label: "Option Y", value: "OPTION Y"},
+                                {label: "Option Z", value: "OPTION Z"}
+                            ]
+                        };
+                        break;
+                }
                 FieldsService.createFieldForForm(formId, field)
                     .then(function(response) {
                         init();
-                    })
+                    });
             }
         }
 
