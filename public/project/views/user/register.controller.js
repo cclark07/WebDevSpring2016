029c0843de77881971919e46158b5370a9da3b0f
Chris Clark <c.clark1928@gmail.com>
2016-03-24 19:39:11 -0400

    Refactor registration controller as vm instance

--- public/project/views/user/register.controller.js
+++ public/project/views/user/register.controller.js
@@ -4,28 +4,29 @@
         .module("SourceCamApp")
         .controller("RegisterController", RegisterController);
 
-    function RegisterController($rootScope, $scope, $location, UserService) {
+    function RegisterController($rootScope, $location, UserService) {
+        var vm = this;
         // Inject register function into scope
-        $scope.register = register;
+        vm.register = register;
 
         // Controller variables to be bound to view inputs
-        $scope.username;
-        $scope.password;
-        $scope.verify;
-        $scope.firstname;
-        $scope.lastname;
-        $scope.email;
+        vm.username;
+        vm.password;
+        vm.verify;
+        vm.firstname;
+        vm.lastname;
+        vm.email;
 
         // Uses the UserService to create the new user
         // Store the new user object in the $rootScope as currentUser
         // Use the $location service to navigate to the profile view
         function register() {
             var newUser = {
-                "username":$scope.username,
-                "password":$scope.password,
-                "firstName":$scope.firstname,
-                "lastName":$scope.lastname,
-                "email":$scope.email,
+                "username":vm.username,
+                "password":vm.password,
+                "firstName":vm.firstname,
+                "lastName":vm.lastname,
+                "email":vm.email,
                 "roles": ["User"]
             };
             UserService.createUser(newUser, function(response) {
