5089cccd57806585418c062f3c723fff53114940
Chris Clark <c.clark1928@gmail.com>
2016-03-24 19:35:41 -0400

    Refactor login controller as vm instance

--- public/project/views/user/login.controller.js
+++ public/project/views/user/login.controller.js
@@ -4,18 +4,19 @@
         .module("SourceCamApp")
         .controller("LoginController", LoginController);
 
-    function LoginController($rootScope, $scope, $location, UserService) {
+    function LoginController($rootScope, $location, UserService) {
+        var vm = this;
         // Inject login function into scope
-        $scope.login = login;
+        vm.login = login;
 
         // Controller variables bound to view inputs
-        $scope.username;
-        $scope.password;
+        vm.username;
+        vm.password;
 
         // Attempt to login with input values
         // If the user exists, it's stored in the $rootScope and user is re-routed to profile page
         function login() {
-            UserService.findUserByCredentials($scope.username, $scope.password, function(response) {
+            UserService.findUserByCredentials(vm.username, vm.password, function(response) {
                 if (response) {
                     $rootScope.currentUser = response;
                     $location.path("/profile");
