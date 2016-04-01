1a9a87866b5e0ec517bcf5fce60d3083c1d04cef
Chris Clark <c.clark1928@gmail.com>
2016-03-31 21:04:23 -0400

    Create mongoose and db objects and inject them into server/app.js

--- server.js
+++ server.js
@@ -2,12 +2,33 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var uuid = require('node-uuid');
 var app = express();
+var session = require('express-session');
+// install and require the mongoose library
+var mongoose = require('mongoose');
+
+var connectionString = 'mongodb://127.0.0.1:27017/formmakerdb';
+
+// use remote connection string
+// if running in remote server
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+}
+
+// connect to the database
+var db = mongoose.connect(connectionString);
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-require("./public/assignment/server/app.js")(app, uuid);
+
+require("./public/assignment/server/app.js")(app, uuid, db, mongoose);
 require("./public/project/server/app.js")(app, uuid);
 app.listen(port, ipaddress);
 
