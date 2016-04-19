bb0ece31d5df1ce062cf6dd4e94a0030f0d025ac
Chris Clark <c.clark1928@gmail.com>
2016-04-19 16:34:11 -0400

    Populate map with location markers linked to webcam urls

--- public/project/client/views/worldmap/worldmap.controller.js
+++ public/project/client/views/worldmap/worldmap.controller.js
@@ -6,6 +6,7 @@
 
     function MapController($location, LocationService) {
         var vm = this;
+        var markerLinks = [];
 
         vm.locations = [];
 
@@ -16,22 +17,41 @@
             LocationService.getAllLocations()
                 .then(function(response) {
                     vm.locations = response.data;
+                    initMap();
                 });
         }
 
         function initMap() {
+            markerLinks = [];
+
             var mapProp = {
                 center:new google.maps.LatLng(42.508742,-71.120850),
                 zoom:2,
                 mapTypeId:google.maps.MapTypeId.HYBRID
             };
             var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+            for (var i = 0; i < vm.locations.length; i++) {
+                var marker = new google.maps.Marker({
+                    position: {lat: vm.locations[i].lat, lng: vm.locations[i].lon},
+                    map: map,
+                    title: vm.locations[i].name
+                });
+
+                markerLinks[i] = {marker: marker, url: vm.locations[i].webcamURL};
+                addListener(i);
+            }
         }
         google.maps.event.addDomListener(window, 'load', initMap);
 
         if ($location.url() == '/home') {
             getLocations();
-            initMap();
+        }
+
+        function addListener(index) {
+            markerLinks[index].marker.addListener('click', function() {
+                window.open(markerLinks[index].url);
+            });
         }
     }
 })();
