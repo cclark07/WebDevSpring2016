dd608d24ae405325ff453854514217a95887596b
Chris Clark <c.clark1928@gmail.com>
2016-04-19 17:04:41 -0400

    Show location on map in location page

--- public/project/client/views/location/location.controller.js
+++ public/project/client/views/location/location.controller.js
@@ -14,15 +14,38 @@
 
         vm.addComment = addComment;
         vm.favoriteLocation = favoriteLocation;
+        vm.initMap = initMap;
 
         function init() {
             LocationService.getLocationById(locationId)
                 .then(function(response) {
                     vm.location = response.data;
+                    initMap();
                 })
         }
 
         init();
+
+        function initMap() {
+
+            var mapProp = {
+                center:new google.maps.LatLng(vm.location.lat,vm.location.lon),
+                zoom:5,
+                mapTypeId:google.maps.MapTypeId.HYBRID
+            };
+            var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+            var marker = new google.maps.Marker({
+                position: {lat: vm.location.lat, lng: vm.location.lon},
+                map: map,
+                title: vm.location.name
+            });
+
+            marker.addListener('click', function() {
+                window.open(vm.location.webcamURL);
+            });
+        }
+        google.maps.event.addDomListener(window, 'page:load', initMap);
 
         function addComment() {
             if (!$rootScope.currentUser) {
